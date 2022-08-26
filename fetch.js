932f4cf4f19703d9ccd2180d91faa985ceb236e6
Md Rubel Ahmed Rana <mdrubelahmedrana521@gmail.com>
2022-08-27 01:17:07 +0600

    My first API project done

---
+++ fetch.js
@@ -0,0 +1,23 @@
+
+const displayData = (data) => {
+    console.log(data);
+    const postContainer = document.getElementById("post-container");
+    for (const user of data) {
+        const postDiv = document.createElement("div");
+        postDiv.classList.add("post");
+        postDiv.innerHTML = `
+            <h2>UserID: ${user.id}</h2>
+            <h4>Title: ${user.title}</h4>
+            <p> ${user.body} </p>
+        `;
+        postContainer.appendChild(postDiv)
+    }
+} 
+
+const loadData = () => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(res => res.json())
+        .then(data => displayData(data))
+}
+
+loadData()
